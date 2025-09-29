e3c62551cb63ab488799049b5a8e6610e3e0da44
2024olzh <2024olzh@gmail.com>
2025-09-29 16:19:51 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+import pandas as pd
+import streamlit as st
+
+# 제목
+st.title("MBTI 국가별 데이터 미리보기")
+
+# CSV 파일 불러오기 (같은 폴더에 있다고 가정)
+df = pd.read_csv("countriesMBTI_16types.csv")
+
+# 상위 5줄만 표시
+st.subheader("데이터 상위 5줄")
+st.dataframe(df.head())
 
